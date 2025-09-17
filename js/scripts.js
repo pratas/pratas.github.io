be5b0411fb96a1fb390478ea242553e7195adcd5
Diogo Pratas <6882743+pratas@users.noreply.github.com>
2025-09-17 08:47:47 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-document.getElementById('toggleAlumni').addEventListener('click', function() {
-  const list = document.getElementById('alumniList');
-  list.classList.toggle('hidden');
-  this.textContent = list.classList.contains('hidden')
-    ? 'Show Alumni'
-    : 'Hide Alumni';
-});
-
 document.getElementById('toggleBiography').addEventListener('click', function() {
   const list = document.getElementById('biography');
   list.classList.toggle('hidden');
